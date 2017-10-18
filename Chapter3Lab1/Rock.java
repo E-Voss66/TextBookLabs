de5db0f47a22793185fc281e562232cb489eece4
Shimi66 <31352848+E-Voss66@users.noreply.github.com>
2017-10-18 09:31:55 -0700

    Chapter 3 problems through 4

--- Chapter3Lab1/Rock.java
+++ Chapter3Lab1/Rock.java
@@ -12,12 +12,21 @@ public class Rock
     {
         String personPlay;    //User's play -- "R", "P", or "S"
         String computerPlay;  //Computer's play -- "R", "P", or "S"
-        int computerInt;      //Randomly generated number used to determine
-                              //computer's play
-        Random generator = new Random();
-        Scanner scan = new Scanner(System.in);
+        int result;                     
         
+        System.out.println();
+        System.out.println("Choose Rock, Paper, or Scissors");
         
+        personPlay = getPlayersChoice();
+        computerPlay = generateComputerChoice();
+        
+        System.out.println("Your play is: " + personPlay);
+        System.out.println("The computer play is: " + computerPlay);
+        
+        // If you win, it returns 1, tie it returns 0, lose returns -1
+        result = decideWinner(personPlay, computerPlay);
+        
+        printResult(result);
         
         //Get player's play -- note that this is stored as a string
         //Make player's play uppercase for ease of comparison
@@ -25,5 +34,49 @@ public class Rock
         //Translate computer's randomly generated play to string
         //Print computer's play
         //See who won.
+    }
+    
+    public static String getPlayersChoice(){
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+    
+    public static String generateComputerChoice(){
+        int computerValue;
+        Random generator = new Random();
+        
+        computerValue = generator.nextInt(3);
+        
+        if(computerValue == 0){
+            return "Rock";
+        } 
+        else if (computerValue == 1){
+            return "Scissors";
+        }
+        else{
+            return "Paper";
+        }
+        
+    }
+    
+    public static int decideWinner(String player, String computer){
+        if(player.equals(computer)){
+            return 0;
+        } else if(player.equals("Rock") && computer.equals("Scissors") || player.equals("Scissors")
+        && computer.equals("Paper") || player.equals("Paper") && computer.equals("Rock")){
+            return 1;
+        } else{
+            return -1;
+        }
+    }
+    
+    public static void printResult(int result){
+        if(result == 1){
+            System.out.println("You Won");
+        } else if (result == 0){
+            System.out.println("You tied");
+        } else{
+            System.out.println("You lost");
+        }
     }
 }
