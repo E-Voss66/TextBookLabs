bc600013a448f3dfa3761c80fa23964f59fad8f2
Shimi66 <31352848+E-Voss66@users.noreply.github.com>
2017-10-23 11:56:59 -0700

    Chapt 3 Lab

--- Chapter3Lab1/StarVariations.java
+++ Chapter3Lab1/StarVariations.java
@@ -38,12 +38,18 @@ public class StarVariations
         System.out.println("Enter how long you want the pattern");
         longestLine = scanB.nextInt();
         
-        while(currentLine <= longestLine){
-            for(int i = currentLine; i > 0 ; i --){
-                System.out.print("*");
-            }
+        for( int i = longestLine; i > 0; i--){
+            printCharacter(" ", i - 1);
+            printCharacter("*", currentLine);
             System.out.println();
             currentLine += 1;
+        }
+        
+    }
+    
+    public static void printCharacter(String character, int repeat){
+        for( int i = repeat; i > 0; i--){
+            System.out.print(character);
         }
     }
     
@@ -56,6 +62,7 @@ public class StarVariations
         System.out.println("Enter how long you want you want the pattern");
         longestLine = scanC.nextInt();
         
+        // way too complicated version of what I did in the problem before
         while(currentLine < longestLine){
             for(int i = currentLine; i < longestLine; i++){
                 while(x){
@@ -70,6 +77,47 @@ public class StarVariations
             x = true;
             System.out.println();
             currentLine += 1;
+        }
+    }
+    
+    public static void createD(){
+        Scanner scanD = new Scanner(System.in);
+        int lengthOfPattern;
+        int longestLine;
+        int currentLine = 1;
+        
+        System.out.println("Enter how long you want the pattern (greater than 3)");
+        lengthOfPattern = scanD.nextInt();
+        
+        if(lengthOfPattern % 2 == 1){
+            longestLine = lengthOfPattern;
+        }
+        else {
+            longestLine = lengthOfPattern - 1;
+        }
+        
+        int test = lengthOfPattern;
+        
+        for(int i = lengthOfPattern; i > 0; i--){
+            printCharacter(" ", Math.abs(lengthOfPattern)/2);
+            printCharacter("*", starRepititions(currentLine, test, lengthOfPattern));
+            System.out.println();
+            currentLine += 1;
+            lengthOfPattern -= 2;
+        }
+        
+    }
+    
+    public static int starRepititions(int currentLine, int test, int lengthPattern){
+        int FIX = Math.abs(lengthPattern);
+        System.out.println("test is " + test);
+        System.out.println("lengthPattern is " + lengthPattern);
+        System.out.println("Fix is  " + FIX);
+        if(lengthPattern > 0){
+            return -1 + 2 * currentLine;
+        }
+        else{
+            return test - FIX + 1;
         }
     }
 }
